5d7bc11c85a783820672735622b4cb3ce9a07312
Mohamed Mesahel <82197617+MohamedMesahel@users.noreply.github.com>
2021-09-13 20:57:39 -0700

    creating workout model

--- models/Workout.js
+++ models/Workout.js
@@ -1,5 +1,45 @@
 // TODO: Build Workout model and make it ready for export
-const Workout = require('./Workout');
+// TODO: require mongos and build the schema
 
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-module.exports = { Workout };
+const WorkoutSchema = new Schema(
+    {
+        day: {type: Date, default: () => new Date()},
+        exercises: [
+            {
+                type: {
+                    type: String,
+                },
+                name: {
+                    type: String,
+                },
+                duration: {
+                    type: Number,
+                },
+                weight: Number,
+                reps: Number,
+                sets: Number,
+                distance: Number,
+            },
+            
+        ],
+    },
+    {
+        toJSON: {
+            virtual: true,
+        },
+    }
+);
+
+WorkoutSchema.virtual('totalDuration').get(function() {
+    const duration = this.exercises.reduce((acc, curr) => {
+        return acc + cur.duration;
+    }, 0);
+    return duration;
+});
+
+const Workout = mongose.model('Workout', WorkoutSchema);
+
+module.exports = Workout
