3b12dc558aed010e5dfb7af54d14cf76158e43f3
Mohamed Mesahel <82197617+MohamedMesahel@users.noreply.github.com>
2021-09-14 09:32:12 -0700

    test appRoutes

--- routes/appRoutes.js
+++ routes/appRoutes.js
@@ -2,34 +2,57 @@
 // TODO: Get all app routes
 // Mongoose queries are not promises. They have a .then() function for co and async/await as a convenience.
 const { Workout } = require('../models/Workout');
-const express = require('express');
 const router = require('express').Router();
 
 // TODO: Get all workout routes
 router.get("/api/workouts", (req, res) => {
     Workout.find({})
-    .then((dbWorkout) => {
-        res.json(dbWorkout);
-    });
-    .catch((err) => {
-        res.json(err)
-    });
-    
+        .then((dbWorkout) => {
+            res.json(dbWorkout);
+        })
+        .catch((err) => {
+            res.json(err);
+        });
 });
 
 // Create workout 
-router.post('/api/workouts', async (req, res) => {
-    try {
-        const workoutsData = await Workout.create({
-            ...req.body,
+router.post("/api/workouts", ({ body }, res) => {
+    Workout.create({})
+        .then((dbWorkout) => {
+            res.json(dbWorkout);
+        })
+        .catch(({ message }) => {
+            console.log(message);
         });
-        res.status(200).json(workoutsData);
-    } catch (err) {
-        res.status(500).json(err);
-    }
 });
 
 // TODO: Get and update one workout 
+router.put("/api/workouts/:id", ({ params, body }, res) => {
+    console.log("PARAMS", body, params);
+
+    Workout.findOneAndUpdate(
+        { _id: params.id },
+        { $push: { exercises: body } },
+        { new: true }
+    )
+        .then((dbWorkout) => {
+            res.json(dbWorkout);
+        })
+        .catch((err) => {
+            res.json(err);
+        });
+});
 // TODO: Get one workout/range
 
+router.get("/api/workouts/range", (req, res) => {
+    Workout.find({})
+        .limit(7)
+        .then((dbWorkout) => {
+            res.json(dbWorkout);
+        })
+        .catch((err) => {
+            res.json(err);
+        });
+});
 
+module.exports = router;
